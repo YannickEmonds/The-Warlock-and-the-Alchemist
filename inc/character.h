8b1717ba5b04b574024f646f87cfb26ea1d34467
Yannick <yannick.emonds@ziti.uni-heidelberg.de>
2022-10-01 16:28:08 +0200

    basic character definitions

---
+++ inc/character.h
@@ -0,0 +1,46 @@
+#include <vector>
+#include <string>
+#include "action.h"
+
+
+enum class Race
+{
+    human, elf, dwarf, orc
+};
+
+
+struct CharacterStats
+{
+    unsigned strength;
+    unsigned dexterity;
+    unsigned agility;
+    unsigned initiative;
+};
+
+
+class Character
+{
+private:
+    const std::string name;
+    const Race race;
+    unsigned healthPoints;
+    unsigned stamina;
+    CharacterStats stats;
+    std::vector<Action> actions;
+public:
+    std::string getName() const;
+};
+
+
+class Mage : private Character
+{
+private:
+    unsigned mana;
+};
+
+
+class Warlock : private Mage
+{
+public:
+    Warlock(std::string name);
+};
