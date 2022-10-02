9c6f2725a819a523bef213199808185d9fdb8ded
Yannick <yannick.emonds@ziti.uni-heidelberg.de>
2022-10-02 18:56:43 +0200

    further menu development

--- inc/menu.h
+++ inc/menu.h
@@ -1,28 +1,35 @@
 #include <vector>
 #include <string>
+#include <memory>
+#include <map>
 
 
 class Option
 {
 private:
     const std::string name;
+    const std::shared_ptr<Menu> menu;
 
 public:
-    Option();
+    Option(std::shared_ptr<Menu> _menu);
 
     std::string getName() const;
-
+    void performAction() const;
 };
 
 
 class Menu
 {
 private:
+    const std::string name;
     std::vector<Option> options;
+    std::map<std::string, std::shared_ptr<Menu>> reachableMenus;
 public:
-    Menu();
+    Menu(std::vector<Option> _options, 
+         std::map<std::string, Menu> _reachableMenus);
 
     void show();
     void close();
-    void waitForInput();
+    std::vector<Option> getOptions() const;
+    std::map<std::string, std::shared_ptr<Menu>> getReachableMenus() const;
 };
